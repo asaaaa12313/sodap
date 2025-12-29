4a8279bb8fc26851237d57f9f5ae9b9f86ffd758
김민우 <kimminwoo@gimmin-uui-MacBookAir.local>
2025-12-29 17:56:51 +0900

    fix(content): update address to new location

--- src/data/location.ts
+++ src/data/location.ts
@@ -1,11 +1,11 @@
 export const locationData = {
   // 구글 지도 등록 명칭 및 주소
   name: "맛있는고기에솜씨를더하다 소답중동점",
-  address: "경상남도 창원시 의창구 의창대로 272-1", // 구글맵 기준
-  addressDetail: "1층 (소답동)",
+  address: "경남 창원시 의창구 의창대로261번길 6",
+  addressDetail: "1층",
   zipCode: "51196",
   phone: "0507-1353-9229", // 네이버 스마트콜/구글 등록 번호 확인 필요
   parkingInfo: "유니파크 주차장 2시간 무료 지원",
   // TODO: Replace with actual short link if available
-  googleMapLink: "https://www.google.com/maps/search/?api=1&query=경상남도+창원시+의창구+의창대로+272-1"
+  googleMapLink: "https://www.google.com/maps/search/?api=1&query=경남+창원시+의창구+의창대로261번길+6"
 };
